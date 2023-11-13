bec139bd658b4ffa6944a869dfe3452eea9b15d3
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2023-11-13 21:00:25 +0200

    Finish project-reader

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,21 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, licence, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.licence = licence
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
-    def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+    def _stringify_multiple_entries(self, multi_item):
+        return "\n- ".join(multi_item) if len(multi_item) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.licence}\n"
+            f"\nAuthors:\n- {self._stringify_multiple_entries(self.authors)}\n"
+            f"\nDependencies:\n- {self._stringify_multiple_entries(self.dependencies)}\n"
+            f"\nDevelopment dependencies:\n- {self._stringify_multiple_entries(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import toml
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,19 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
 
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        # deserialisoi
+        kontsa = toml.loads(content)
+        #helpota elämää
+        poetry_dict = kontsa['tool']['poetry']
+
+        # lataa yksittäiset arvot
+        nimi = poetry_dict['name']
+        kuvaus = poetry_dict['description']
+        lisenssi = poetry_dict['license']
+        tekijat = poetry_dict['authors']
+        riippuvuudet = list(poetry_dict['dependencies'].keys())
+        kehitysriippuvuudet = list(poetry_dict['group']['dev']['dependencies'].keys())
+
+        # Muodosta Project-olio tietojen perusteella
+        return Project(nimi, kuvaus, lisenssi, tekijat, riippuvuudet, kehitysriippuvuudet)

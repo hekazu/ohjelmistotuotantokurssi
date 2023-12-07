d4afab871286fcf05e64c63f493bb3964e1048de
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2023-12-07 15:14:43 +0200

    Improve less scalable query language solution

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -10,6 +10,18 @@ class And:
         return True
 
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
+
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
@@ -27,3 +39,24 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+class All:
+    def test(self, player):
+        return True
+
+
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def test(self, player):
+        return not self._matcher.test(player)
+
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._matcher = Not(HasAtLeast(value,attr))
+
+    def test(self, player):
+        return self._matcher.test(player)

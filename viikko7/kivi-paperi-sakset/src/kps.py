077ce463841bc3f9459e0d0605d54d222b38a60e
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2023-12-14 16:00:35 +0200

    Start refactoring KPS

---
+++ viikko7/kivi-paperi-sakset/src/kps.py
@@ -0,0 +1,28 @@
+from tuomari import Tuomari
+
+
+class KiviPaperiSakset:
+    def pelaa(self):
+        tuomari = Tuomari()
+
+        ekan_siirto = self._ensimmaisen_siirto()
+        tokan_siirto = self._toisen_siirto(ekan_siirto)
+
+        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
+            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
+            print("Kiitos!")
+            print(tuomari)
+
+            ekan_siirto = self._ensimmaisen_siirto()
+            tokan_siirto = self._toisen_siirto(ekan_siirto)
+
+    # Tämä on joka pelissä sama, hoituu näin yläluokassa kätevästi
+    def _ensimmaisen_siirto(self):
+        return input("Ensimmäisen pelaajan siirto: ")
+
+    # Tämä taas ei toimi tällaisenaan, mutta toteutetaan vaikka herrasmieskivi
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        return "k"
+
+    def _onko_ok_siirto(self, siirto):
+        return siirto == "k" or siirto == "p" or siirto == "s"
